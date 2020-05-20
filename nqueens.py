a1875530a0cf37d2565425c7a1c62bd3a2b54155
Ronin Winter <roninwinter@gmail.com>
2020-05-20 22:09:57 +0200

    working code

---
+++ nqueens.py
@@ -0,0 +1,301 @@
+import sys
+import random
+import math
+
+from random import choice
+
+MAXQ = 100
+
+
+def in_conflict(column, row, other_column, other_row):
+    """
+    Checks if two locations are in conflict with each other.
+    :param column: Column of queen 1.
+    :param row: Row of queen 1.
+    :param other_column: Column of queen 2.
+    :param other_row: Row of queen 2.
+    :return: True if the queens are in conflict, else False.
+    """
+    if column == other_column:
+        return True  # Same column
+    if row == other_row:
+        return True  # Same row
+    if abs(column - other_column) == abs(row - other_row):
+        return True  # Diagonal
+
+    return False
+
+
+def in_conflict_with_another_queen(row, column, board):
+    """
+    Checks if the given row and column correspond to a queen that is in conflict with another queen.
+    :param row: Row of the queen to be checked.
+    :param column: Column of the queen to be checked.
+    :param board: Board with all the queens.
+    :return: True if the queen is in conflict, else False.
+    """
+    for other_column, other_row in enumerate(board):
+        if in_conflict(column, row, other_column, other_row):
+            if row != other_row or column != other_column:
+                return True
+    return False
+
+
+def count_conflicts(board):
+    """
+    Counts the number of queens in conflict with each other.
+    :param board: The board with all the queens on it.
+    :return: The number of conflicts.
+    """
+    cnt = 0
+
+    for queen in range(0, len(board)):
+        for other_queen in range(queen+1, len(board)):
+            if in_conflict(queen, board[queen], other_queen, board[other_queen]):
+                cnt += 1
+
+    return cnt
+
+
+def evaluate_state(board):
+    """
+    Evaluation function. The maximal number of queens in conflict can be 1 + 2 + 3 + 4 + .. +
+    (nquees-1) = (nqueens-1)*nqueens/2. Since we want to do ascending local searches, the evaluation function returns
+    (nqueens-1)*nqueens/2 - countConflicts().
+
+    :param board: list/array representation of columns and the row of the queen on that column
+    :return: evaluation score
+    """
+    return (len(board)-1)*len(board)/2 - count_conflicts(board)
+
+
+def print_board(board):
+    """
+    Prints the board in a human readable format in the terminal.
+    :param board: The board with all the queens.
+    """
+    print("\n")
+
+    for row in range(len(board)):
+        line = ''
+        for column in range(len(board)):
+            if board[column] == row:
+                line += 'Q' if in_conflict_with_another_queen(row, column, board) else 'q'
+            else:
+                line += '.'
+        print(line)
+
+
+def init_board(nqueens):
+    """
+    :param nqueens integer for the number of queens on the board
+    :returns list/array representation of columns and the row of the queen on that column
+    """
+
+    board = []
+
+    for column in range(nqueens):
+        board.append(random.randint(0, nqueens-1))
+
+    return board
+
+
+"""
+------------------ Do not change the code above! ------------------
+"""
+def random_search(board):
+    """
+    This function is an example and not an efficient solution to the nqueens problem. What it essentially does is flip
+    over the board and put all the queens on a random position.
+    :param board: list/array representation of columns and the row of the queen on that column
+    """
+
+    i = 0
+    optimum = (len(board) - 1) * len(board) / 2
+
+    while evaluate_state(board) != optimum:
+        i += 1
+        print('iteration ' + str(i) + ': evaluation = ' + str(evaluate_state(board)))
+        if i == 1000:  # Give up after 1000 tries.
+            break
+
+        for column, row in enumerate(board):  # For each column, place the queen in a random row
+            board[column] = random.randint(0, len(board)-1)
+
+    if evaluate_state(board) == optimum:
+        print('Solved puzzle!')
+
+    print('Final state is:')
+    print_board(board)
+
+
+# iterates over the neighbouring nodes of the current board
+def nodes(current):
+        nodes = []
+        for row in range(len(current)):
+            for column in range(len(current)):
+                if column != current[row]:
+                    node = list(current)
+                    node[row] = column
+                    nodes.append(tuple(node))
+        return nodes
+
+# steepest ascent version of the hill climbing algorithm
+def hill_climbing(board):
+    
+    i = 0
+    solution = (len(board) - 1) * len(board) / 2
+
+    # evaluate the initial state, if it is the solution then return it
+    if evaluate_state(board) == solution:
+        print('Solved puzzle!')
+        print('Final state is:')
+        print_board(board)
+
+    # else set the current state to the initial state
+    current = board
+
+    # loop until solution state is found
+    while current != solution:
+        i += 1
+        print('iteration ' + str(i) + ': evaluation = ' + str(evaluate_state(board)))
+        if i == 1000:  # Give up after 1000 tries.
+            break
+        
+        # iterates through the neighbours of the current board
+        neighbours = nodes(current)
+        # find the neighbour with the highest value
+        neighbour =  max(neighbours,
+                key=lambda state: (evaluate_state(current), random.random()))
+
+        # if the neighbour has higher heuristic value than the current state
+        if(evaluate_state(neighbour) >= evaluate_state(current)):
+            # then set the current state as the neighbour state
+             current = neighbour
+    
+        # If the current state is the solution then exit
+        if evaluate_state(current) == solution:
+            print('Solved puzzle!')
+            break
+    
+    # return the board configuration
+    print('Final state is:')
+    print_board(current)
+
+
+# schedule function that maps time to temperature 
+def schedule(k=0.1, lam=0.001, limit=20000):
+    return lambda t: (k * math.exp(-lam * t) if t < limit else 0)
+
+# simulated annealing is a version of stochastic hill climbing
+def simulated_annealing(board):
+    i = 0
+    solution = (len(board) - 1) * len(board) / 2
+
+    # evaluate the initial state, if it is the solution then return it
+    if evaluate_state(board) == solution:
+        print('Solved puzzle!')
+        print('Final state is:')
+        print_board(board)
+
+    # else set the current state to the initial state
+    current = board
+
+    # initialise neighbour as a copy of the current baord
+    neighbour = list(current)
+
+    # current value is the heuristic value of the current board
+    currentValue = evaluate_state(current)
+
+    solution = (len(board) - 1) * len(board) / 2
+
+    t = 0
+
+    # loop until solution is found
+    while current != solution:
+        i += 1
+        print('iteration ' + str(i) + ': evaluation = ' + str(evaluate_state(board)))
+        if i == 10000:  # Give up after 10000 tries.
+            break
+        
+        # iteratively increase time
+        t+=1
+
+        # value of temperature as a function of time determined with the schedule function
+        T = schedule(t)
+
+        # if T = 0 or solution is found exit
+        if T == 0 or evaluate_state(current) == solution:
+            print('Solved puzzle!')
+            break
+        
+        # neighbour is a randomly selected successor of current 
+        for column, row in enumerate(current):  # For each column, place the queen in a random row
+            neighbour[column] = random.randint(0, len(board)-1)
+    
+        if not neighbour:
+            print('Solved puzzle!')
+            break
+        
+        # the value of next is the heuristic value of the neighbour states
+        nextValue = evaluate_state(neighbour)
+        
+        deltaE = nextValue - currentValue
+        if deltaE > 0:
+            current = neighbour
+            currentValue = nextValue
+
+    print('Final state is:')
+    print_board(current)
+
+
+def genetic_algorithm(board):
+    pass
+
+
+def main():
+    """
+    Main function that will parse input and call the appropriate algorithm. You do not need to understand everything
+    here!
+    """
+
+    try:
+        if len(sys.argv) != 2:
+            raise ValueError
+
+        n_queens = int(sys.argv[1])
+        if n_queens < 1 or n_queens > MAXQ:
+            raise ValueError
+
+    except ValueError:
+        print('Usage: python n_queens.py NUMBER')
+        return False
+
+    print('Which algorithm to use?')
+    algorithm = input('1: random, 2: hill-climbing, 3: simulated annealing \n')
+
+    try:
+        algorithm = int(algorithm)
+
+        if algorithm not in range(1, 4):
+            raise ValueError
+
+    except ValueError:
+        print('Please input a number in the given range!')
+        return False
+
+    board = init_board(n_queens)
+    print('Initial board: \n')
+    print_board(board)
+
+    if algorithm == 1:
+        random_search(board)
+    if algorithm == 2:
+        hill_climbing(board)
+    if algorithm == 3:
+        simulated_annealing(board)
+
+
+# This line is the starting point of the program.
+if __name__ == "__main__":
+    main()
